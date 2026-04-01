e2fbaf8edbfbc3eba7f81aa536ba09d892dfe413
peeraphat <flookzajajuz2@gmail.com>
2026-04-01 16:16:34 +0700

    bug fix#3 - revise candidade no. validation

--- validate.js
+++ validate.js
@@ -9,26 +9,37 @@ function checkSID() {
 
 function checkCandiNo() {
   let candi = (document.getElementById("candi").value).trim();
-  if (isNaN(candi) || candi < 1) {
+
+
+  if (isNaN(candi)) {
     return false;
-  } else {
-    return true;
   }
+
+ 
+  let num = parseInt(candi);
+
+ 
+  if (num < 1 || num > 10) {
+    return false;
+  }
+
+  return true;
 }
 
 function validateForm(){
-	if(!checkSID()){
-	  alert("Invalid value for Student ID!");
-	  document.getElementById("sid").focus();
-	  return false;
-	}else{
-		if(!checkCandiNo()){
-		  alert("Invalid value for Candidate No!!");
-		  document.getElementById("candi").focus();
-		  return false;
-		}else{
-			alert("Your input data passes validation!!");
-			return true;
-		}
-	}
+
+    if(!checkSID()){
+        alert("Invalid value for Student ID!");
+        document.getElementById("sid").focus();
+        return false;
+    }
+
+    if(!checkCandiNo()){
+        alert("Invalid value for Candidate No!!");
+        document.getElementById("candi").focus();
+        return false;
+    }
+
+    alert("Your input data passes validation!!");
+    return true;
 }
